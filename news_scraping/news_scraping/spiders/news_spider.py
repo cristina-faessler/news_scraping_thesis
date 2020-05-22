b6c2acfd61b79e1a6d21fb9d4a325cd3a073ec1f
Cristina Bolohan <cristina.bolohan@irt.de>
2020-05-22 16:41:43 +0200

    Initial commit

---
+++ news_scraping/news_scraping/spiders/news_spider.py
@@ -0,0 +1,28 @@
+#extracted Data - Temporary containers (items) - storing in database
+import scrapy
+from urllib.parse import urljoin
+from ..items import NewsScrapingItem
+
+class NewsSpider(scrapy.Spider):
+
+    name = 'news'
+    start_urls = ['https://www.tagesschau.de/']
+
+    #parse function
+    def parse(self, response):
+        for href in response.css("div.teaser > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_news_article)
+
+    def parse_news_article(self, response):
+            items = NewsScrapingItem()
+
+            headline = response.css('.headline::text').extract()
+            date_publish = response.css('.stand').get()
+            article_text = response.css('p.text.small::text').extract()
+
+            items['headline'] = headline
+            items['date_publish'] = date_publish
+            items['article_text'] = article_text
+
+            yield items
